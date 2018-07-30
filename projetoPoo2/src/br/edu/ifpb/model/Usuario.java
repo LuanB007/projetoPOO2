33cf3945f14f945df3dc9226f268705295e41b76
Luan <luanbruno974@gmail.com>
2018-07-30 11:04:30 -0300

    Modelando classe Usuario

---
+++ projetoPoo2/src/br/edu/ifpb/model/Usuario.java
@@ -0,0 +1,62 @@
+package br.edu.ifpb.model;
+
+import java.util.Objects;
+/**
+ *
+ * @author Luan
+ */
+public class Usuario {
+    private String user;
+    private String pass;
+
+    public Usuario(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.user);
+        hash = 67 * hash + Objects.hashCode(this.pass);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.user, other.user)) {
+            return false;
+        }
+        if (!Objects.equals(this.pass, other.pass)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
