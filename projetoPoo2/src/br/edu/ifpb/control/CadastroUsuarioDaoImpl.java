6cba5f6427ecf9c93acd26521a7d3d4d408dc823
Luan <luanbruno974@gmail.com>
2018-08-28 14:50:55 -0300

    Javadoc do crud de usuário.

--- projetoPoo2/src/br/edu/ifpb/control/CadastroUsuarioDaoImpl.java
+++ projetoPoo2/src/br/edu/ifpb/control/CadastroUsuarioDaoImpl.java
@@ -6,27 +6,46 @@ import java.util.List;
 import java.util.Set;
 
 /**
- *
+ * Essa classe contém os métodos necessários para fazer o CRUD de usuário(funcionário).
  * @author Luan
  */
 public class CadastroUsuarioDaoImpl implements CadastroUsuarioDao {
     
     private Set<Funcionario> funcionarios;
-
+    
+    /**
+     * Contrutor da lista de funcionários.
+     */
     public CadastroUsuarioDaoImpl() {
         this.funcionarios = new HashSet<>();
     }
     
+    /**
+     * Método usado para verificar a autenticação do usuário.
+     * @param user o usuario
+     * @param pass a senha
+     * @return true ou false
+     */
     @Override
     public boolean autentica(String user, String pass){
         return funcionarios.stream().anyMatch((f) -> (f.getUsuario().equals(user) && f.getSenha().equals(pass)));
     }
     
+    /**
+     * Método usado para salvar o funcionário na lista.
+     * @param funcionario objeto funcionário.
+     * @return true ou false
+     */
     @Override
     public boolean salvar(Funcionario funcionario){
         return funcionarios.add(funcionario);
     }
     
+    /**
+     * Método usado para deletar um funcionário.
+     * @param cpf CPF do funcionário.
+     * @return true ou false
+     */
     @Override
     public boolean deletar(String cpf){
         if(existeFuncionario(cpf)){
@@ -40,6 +59,12 @@ public class CadastroUsuarioDaoImpl implements CadastroUsuarioDao {
         return false;
     }
     
+    /**
+     * Método que atualiza os dados de um funcionário já presente na lista.
+     * @param funcionario objeto funcionário.
+     * @param cpf CPF do funcionário.
+     * @return 
+     */
     @Override
     public boolean atualizar(Funcionario funcionario, String cpf){
         if(existeFuncionario(cpf)){
@@ -54,11 +79,20 @@ public class CadastroUsuarioDaoImpl implements CadastroUsuarioDao {
         return false;
     }
     
+    /**
+     * Método utilizado para verificar se o usuário já está cadastrado.
+     * @param cpf CPF do funcionário.
+     * @return true ou false.
+     */
     @Override
     public boolean existeFuncionario(String cpf){
         return funcionarios.stream().anyMatch((f) -> (f.getCpf().equals(cpf)));
     }
     
+    /**
+     * Método utilizado para listar todos os funcionários cadastrados.
+     * @return a lista de funcionários.
+     */
     @Override
     public Set<Funcionario> listar(){
         return funcionarios;
